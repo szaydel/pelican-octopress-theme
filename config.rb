b8a032b71730ba4314a424da3517b4961c260245
Sam Zaydel <szaydel@gmail.com>
2013-04-15 21:09:31 -0700

    Catching-up with several changes that should have been committed long ago.

--- config.rb
+++ config.rb
@@ -1,4 +1,6 @@
 # config file for compass
+require 'compass'
+
 project_type = :stand_alone
 
 http_images_path = "../images"
@@ -8,4 +10,4 @@ sass_dir = "sass"
 images_dir = "static/images"
 
 line_comments = false
-output_style = :compressed
+output_style = :nested
